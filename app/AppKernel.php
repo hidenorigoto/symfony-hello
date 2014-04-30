308231f27565d1c5f16b2758296a6aab1be1fc5b
hidenorigoto <hidenorigoto@gmail.com>
2014-04-30 22:56:15 +0900

    added JMSDiExtraBundle, PHPMentorsValidatorBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,7 +16,10 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new JMS\DiExtraBundle\JMSDiExtraBundle($this),
+            new JMS\AopBundle\JMSAopBundle(),
             new Example\HelloBundle\ExampleHelloBundle(),
+            new PHPMentors\ValidatorBundle\PHPMentorsValidatorBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
